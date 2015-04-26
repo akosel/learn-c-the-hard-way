c88a96810255f28b5d5c393bf0534a435f87defc
Aaron Kosel <a_kosel@yahoo.com>
2015-04-26 16:38:29 -0400

    First exercises for LCTHW.

---
+++ ex3/ex3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  char* age = "String";
+  int height = 72;
+
+  age++;
+  printf("I am %s years old.\n", age);
+  printf("I am %d inches tall.\n", height);
+  return 0;
+}
---
+++ ex4/ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  int i = 0;
+  for (i = 0; i < 5; i += 1) {
+    puts("Hello world.");
+  }
+
+  return 0;
+}
